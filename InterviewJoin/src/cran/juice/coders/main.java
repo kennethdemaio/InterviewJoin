336b96680b39af7477adfe8a775d0f4c32f40c6e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-17 18:18:12 -0500

    Uploading project

---
+++ InterviewJoin/src/cran/juice/coders/main.java
@@ -0,0 +1,45 @@
+package cran.juice.coders;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin implements Listener{
+	
+	@Override
+	public void onEnable() {
+		
+		System.out.println("Test plugin enabled");
+		
+		this.getConfig().options().copyDefaults();
+		saveDefaultConfig();	
+		
+		Bukkit.getPluginManager().registerEvents(this, this);
+		
+	}
+		
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		
+		Player p = e.getPlayer();
+		
+		if (!p.hasPermission("InterviewJoin.allow")){
+			e.setCancelled(true);
+			
+		}
+		
+		if(p.isOp()){
+			e.setCancelled(false);
+			
+		}
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("Test plugin disabled");
+		
+	}
+}
